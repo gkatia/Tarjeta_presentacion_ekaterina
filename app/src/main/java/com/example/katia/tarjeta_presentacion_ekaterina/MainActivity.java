4b4aa39739e653baeed8f2e91c816352aeeb2878
Ekaterina Gonzalez <Ekaterina Gonzalez>
2016-10-18 15:41:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/katia/tarjeta_presentacion_ekaterina/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.katia.tarjeta_presentacion_ekaterina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
